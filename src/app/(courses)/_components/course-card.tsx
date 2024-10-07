12ffb334271984d80ee1c0dbd3234ba4f52b4d26
orgamiryazdani <orgamiryazdani@gmail.com>
2024-10-08 01:10:49 +0330

    Add the price component

--- src/app/(courses)/_components/course-card.tsx
+++ src/app/(courses)/_components/course-card.tsx
@@ -1,5 +1,6 @@
 import { Badge } from "@/app/_components/badge";
 import { IconArrowLeftFill, IconClock } from "@/app/_components/icons/icons";
+import { Price } from "@/app/_components/price/price";
 import { CourseSummary } from "@/types/course-summery.interface";
 import Image from "next/image";
 import Link from "next/link";
@@ -57,11 +58,15 @@ export const CourseCard: React.FC<CourseCardProps> = ({
       <div className='card-body'>
         <Link href={`/course/${slug}`}>{title}</Link>
         <p>{subTitle}</p>
-        <div>
+        <div className="flex items-center justify-between mt-3">
           <Badge variant='warning'>
-            <IconClock width={16} height={16}/> {duration}
+            <IconClock
+              width={16}
+              height={16}
+            />{" "}
+            {duration}
           </Badge>
-          {basePrice}
+          <Price price={basePrice} size="small"/>
         </div>
       </div>
       <Link
