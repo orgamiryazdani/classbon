f317de22120c133f191c106a7b97e6c29ada93ce
orgamiryazdani <orgamiryazdani@gmail.com>
2024-10-08 00:25:20 +0330

    Adding icons and making icon packs

--- src/app/(courses)/_components/course-card.tsx
+++ src/app/(courses)/_components/course-card.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/app/_components/badge";
+import { IconArrowLeftFill, IconClock } from "@/app/_components/icons/icons";
 import { CourseSummary } from "@/types/course-summery.interface";
 import Image from "next/image";
 import Link from "next/link";
@@ -57,14 +58,17 @@ export const CourseCard: React.FC<CourseCardProps> = ({
         <Link href={`/course/${slug}`}>{title}</Link>
         <p>{subTitle}</p>
         <div>
-          <Badge variant='warning'>{duration}</Badge>
+          <Badge variant='warning'>
+            <IconClock width={16} height={16}/> {duration}
+          </Badge>
           {basePrice}
         </div>
       </div>
       <Link
         href={`/course/${slug}`}
-        className='card-footer justify-center'>
+        className='card-footer justify-center animated-icon'>
         مشاهده جزئیات دوره
+        <IconArrowLeftFill fill='currentColor' />
       </Link>
     </div>
   );
