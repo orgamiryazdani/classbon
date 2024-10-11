8f004f328180a86d0573d960f93636f515a3da9d
orgamiryazdani <orgamiryazdani@gmail.com>
2024-10-11 12:05:37 +0330

    Managing errors in receiving comments

--- src/app/(courses)/courses/[slug]/_apis/get-comments.ts
+++ src/app/(courses)/courses/[slug]/_apis/get-comments.ts
@@ -1,6 +1,6 @@
 import { readData } from "@/core/http-service/http-service";
 import { CourseCommentList } from "../_types/course-comment.interface";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 type GetCommentsOptions = {
     params: {
@@ -16,12 +16,14 @@ const getComments = ({ params }: GetCommentsOptions): Promise<CourseCommentList>
 }
 
 export const useCourseComments = ({ params }: GetCommentsOptions) => {
-    const { data, isLoading } = useQuery({
-        queryKey: ['courseComments'],
-        queryFn: () => getComments({ params }),
+    const { data, error, fetchNextPage, hasNextPage, refetch,isFetching } = useInfiniteQuery({
+        queryKey: ['courseComments', params.slug],
+        queryFn: ({ pageParam }) => getComments({ params: { ...params, page: pageParam } }),
+        getNextPageParam: (lastPage) => lastPage.nextPage,
+        initialPageParam: 1,
         staleTime: 5 * 60 * 60 * 1000,
         gcTime: 6 * 60 * 60 * 1000
     })
 
-    return { data, isLoading };
+    return { data, error, fetchNextPage, hasNextPage, refetch,isFetching };
 }
